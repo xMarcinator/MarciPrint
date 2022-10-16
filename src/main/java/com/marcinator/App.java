7fef9f0cbbd7cedf4c2d9fa5e427c92d8554e41a
Marc Jensen <marcjensenvirklund@gmail.com>
2022-10-16 18:13:07 +0200

    added fleet run profiles

--- src/main/java/com/marcinator/App.java
+++ src/main/java/com/marcinator/App.java
@@ -5,13 +5,15 @@ import com.marcinator.prettyprint.*;
 import java.awt.Color;
 import com.aventrix.jnanoid.jnanoid.*;
 
+import static java.awt.Color.decode;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		Color foo = Color.decode("#2CC846");
+		Color foo = decode("#2CC846");
 
 		ColorAttribute attribute = new ColorAttribute(foo);
 		ColorAttribute back = new ColorAttribute(200, 0, 0);
